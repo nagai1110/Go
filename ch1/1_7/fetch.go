d2ec0611f6204c25fd5c9bf921d9929d06d4de90
nagai1110 <takaya.nagai@jrits.ricoh.co.jp>
2017-03-19 21:08:54 +0900

    Add exercise1.7

---
+++ ch1/1_7/fetch.go
@@ -0,0 +1,35 @@
+// Copyright © 2017 Takaya.Nagai All Rights Reserved.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		isSucceeded := fetch(url)
+		if !isSucceeded {
+			os.Exit(1)
+		}
+	}
+}
+
+func fetch(url string) bool {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+		return false
+	}
+
+	_, err = io.Copy(os.Stdout, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+		return false
+	}
+
+	return true
+}
