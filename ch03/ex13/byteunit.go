fb7da8831218284bca7cf85dbb8b7ec706b7bfc2
nagai1110 <takaya.nagai@jrits.ricoh.co.jp>
2017-05-19 21:40:43 +0900

    Add exercise3.13

---
+++ ch03/ex13/byteunit.go
@@ -0,0 +1,14 @@
+// Copyright © 2017 Takaya.Nagai All Rights Reserved.
+package main
+
+const (
+	magnification = 1000
+	KB            = 1 * magnification
+	MB            = KB * magnification
+	GB            = MB * magnification
+	TB            = GB * magnification
+	PB            = GB * magnification
+	EB            = PB * magnification
+	ZB            = EB * magnification
+	YB            = ZB * magnification
+)
